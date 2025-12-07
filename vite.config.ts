4a59d9f7a24242b29d36785398e43f8011734617
jeeyo <jeeyo@outlook.com>
2025-12-07 11:06:04 +0700

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,37 +2,37 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   manifest: {
-    //     id: 'money-hater',
-    //     name: 'Money Hater',
-    //     short_name: 'MoneyHater',
-    //     description: 'Expense Tracker',
-    //     start_url: '.',
-    //     theme_color: '#ffffff',
-    //     background_color: '#ffffff',
-    //     display: 'standalone',
-    //     icons: [
-    //       {
-    //         src: '/icon-192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png'
-    //       },
-    //       {
-    //         src: '/icon-512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png'
-    //       }
-    //     ]
-    //   }
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        id: 'wordle-optimizer',
+        name: 'Wordle Optimizer',
+        short_name: 'Wordle Optimizer',
+        description: 'Wordle Optimizer',
+        start_url: '.',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
   ],
   resolve: {
     alias: {
